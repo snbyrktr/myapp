739c6069f729547fbd6125cfcd20c18e4aca2b54
sena <snbyrktr@hotmail.com>
2022-06-04 11:08:55 +0300

    JSON verisi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
 
-function App() {
+// tablo basliklari, json dosyasındaki veri adlandirmalarina gore
+const columns = [
+  {
+    dataField: "name",
+    text: "Name",
+    sort: true
+  },
+  {
+    dataField: "capital",
+    text: "Capital",
+    filter: textFilter(),
+    sort: true,
+    headerStyle: (column, colIndex) => {
+      return { width: "60%" };
+    },
+    style: (column, colIndex) => {
+      return { fontSize: "110%" };
+    }
+  },
+  {
+    dataField: "region",
+    text: "Region",
+    filter: textFilter()
+  },
+  {
+    dataField: "flag",
+    text: "Flag"
+  }
+];
+
+// json dosyasinin okunup, countries ve setcountries e atanmasi
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v2/all")
+      .then((Response) => setCountries(Response.data))
+      .catch((error) => console.log({ error }));
+  }, []);
+
+  // countries e atanan verinin tabloya aktarimi
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {countries.map((country) => {
+        return (
+          <div key={country.id}>
+            <BootstrapTable
+              keyField="id"
+              data={countries}
+              columns={columns}
+              filter={filterFactory()}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
